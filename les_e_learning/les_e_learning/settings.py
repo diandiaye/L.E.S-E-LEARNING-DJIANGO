232d3b899b5f82069b41c636566cdbfcac2f9c0c
falloudiakhate <falloudiakhate9@gmail.com>
2020-07-11 14:33:40 +0000

    Security Settings

--- les_e_learning/les_e_learning/settings.py
+++ les_e_learning/les_e_learning/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'satbzr+zge0358(8tx_2)%xex*+#cwdby7vqos1_)iny&qyv2k'
+SECRET_KEY = '***********************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
