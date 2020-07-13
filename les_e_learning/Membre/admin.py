fe3704ca09a95ca34c6b6d0268ebe109352f3efe
falloudiakhate <falloudiakhate9@gmail.com>
2020-07-13 21:43:43 +0000

    Database Settings

--- les_e_learning/Membre/admin.py
+++ les_e_learning/Membre/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Membre)
+admin.site.register(Annonces)
+admin.site.register(Documents)
+admin.site.register(Post)
+admin.site.register(Commentaire)
+admin.site.register(Cours)
+admin.site.register(Membre_Cours)
--- les_e_learning/Membre/models.py
+++ les_e_learning/Membre/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Membre(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    niveau = models.CharField(max_length=50)
+    
+class Cours(models.Model):
+    titre = models.CharField(max_length=50)
+    categorie = models.CharField(max_length=50)
+    niveau = models.CharField(max_length=50)
+    
+class Membre_Cours(models.Model):
+    enroll_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    impression = models.TextField()
+    nombre_like = models.IntegerField()
+    nombre_dislike = models.IntegerField()
+    Membre = models.ForeignKey(Membre, on_delete=models.CASCADE)
+    cours = models.ForeignKey(Cours, on_delete=models.CASCADE)
+    
+class Annonces(models.Model):
+    titre = models.CharField(max_length=50)
+    categorie = models.CharField(max_length=50)
+    contenu = models.TextField()
+    date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    
+class Documents(models.Model):
+    nom = models.CharField(max_length=50)
+    format_fichier = models.CharField(max_length=50)
+    fichier = models.FileField(upload_to=None, max_length=100)
+    cours = models.ForeignKey(Cours, on_delete=models.CASCADE)
+    annonces = models.ForeignKey(Annonces, on_delete=models.CASCADE)
+    
+class Post(models.Model):
+    thematique = models.CharField(max_length=50)
+    contenu = models.CharField(max_length=50)
+    
+class Commentaire(models.Model):
+    contenu = models.TextField()
+    Membre = models.ForeignKey(Membre, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    
+
+    
+    
+    
