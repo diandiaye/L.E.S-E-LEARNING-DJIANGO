64d238c74d34dcfbf2347e2fdf8dc2ff1bbd7dc0
falloudiakhate <falloudiakhate9@gmail.com>
2020-07-17 18:48:51 +0000

    DataBase requests Courses

--- les_e_learning/Cours/urls.py
+++ les_e_learning/Cours/urls.py
@@ -8,8 +8,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
 
-    path('',views.Cours, name = "Cours" ),
-     path('cours_details',views.Cours_details, name = "Cours_details" ),
+    path('',views.Cours_niveau, name = "Cours_niveau" ),
+    path('cm2/categorie',views.Cours_cat, name = "Cours_cat" ),
+    path('cours_details',views.Cours_details, name = "Cours_details" ),
+    path('cours/liste',views.Cours_liste, name = "Cours_liste" ),
     
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- les_e_learning/Cours/views.py
+++ les_e_learning/Cours/views.py
@@ -1,9 +1,45 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from Membre.models import *
 
 # Create your views here.
 
-def Cours(request):
-    return render(request, "Cours/courses.html")
+def Cours_niveau(request):
+    return render(request, "Cours/course_niveau.html")
+
+def Cours_cat(request):
+    categorie = Cours.objects.values('categorie').distinct()
+    som = categorie.count()
+    paginator = Paginator(categorie, 5)
+    page = request.GET.get('page', 1)
+    try:
+            
+            categorie = paginator.page(page)
+      
+    except PageNotAnInteger:
+            categorie = paginator.page(1)
+    except EmptyPage:
+            categorie = paginator.page(paginator.num_pages)
+            
+    return render(request, "Cours/courses.html", locals())
+
+
+def Cours_liste(request):
+    cours = Cours.objects.all()
+    som = cours.count()
+    paginator = Paginator(cours, 12)
+    page = request.GET.get('page', 1)
+    try:
+            
+            cours = paginator.page(page)
+      
+    except PageNotAnInteger:
+            cours = paginator.page(1)
+    except EmptyPage:
+            cours = paginator.page(paginator.num_pages)
+            
+    return render(request, "Cours/cours_liste.html", locals())
 
 def Cours_details(request):
     return render(request, "Cours/course_details.html")
+
