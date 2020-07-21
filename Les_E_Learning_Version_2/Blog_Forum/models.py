a9ee80d2bc24f63d4344ab1a9452187c33ce9f01
Khadim <diakhadimte@gmail.com>
2020-07-21 22:27:19 +0000

    Blog part in Blog_Forum App done

--- Les_E_Learning_Version_2/Blog_Forum/models.py
+++ Les_E_Learning_Version_2/Blog_Forum/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Les tags (mots-clés)
 class Tag(models.Model):
-    word = models.CharField(max_length=300)
+    word = models.CharField(max_length=250)
 
     def __str__(self):
         return self.word
@@ -41,4 +40,4 @@ class Comments(models.Model):
 
     def __str__(self):
         return self.user.username
-    
+
--- Les_E_Learning_Version_2/Blog_Forum/views.py
+++ Les_E_Learning_Version_2/Blog_Forum/views.py
@@ -1,89 +1,93 @@
-
-
 from django.shortcuts import render,redirect,get_object_or_404
-# from Forum.models import *
-# from django.contrib import messages
-# from django.contrib.auth.models import User
-# from django.db.models import Q 
-# from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-# from django.contrib.auth.decorators import login_required
-# from datetime import datetime
+from Blog_Forum.models import *
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.db.models import Q 
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
-def Blog(request):
-    return render(request,'Blog_Forum/blog.html',locals())
-    
-# def posts(request):
-#     query = request.GET.get('q')
-#     posts_list = Post.objects.all()
-#     r_posts = Post.objects.all().order_by('-id')[:4]
-#     if query:
-#         posts_list = Post.objects.filter(
-#             Q(title__icontains=query)|
-#             Q(author__username__icontains=query)|
-#             Q(content__icontains=query)
-#         )
-#     paginator = Paginator(posts_list,6)
-#     page = request.GET.get('page')
-#     try : 
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     if request.method == 'POST':
-#         title = request.POST['title']
-#         content = request.POST['content']
-#         post = Post()
-#         post.author = request.user
-#         post.title = title
-#         post.content = content
-#         post.status = status
-#         try :
-#             post.photo= request.FILES["photo"]
-#         except :
-#             print('Pas de photo de profil')
-#         post.save()
-#         return redirect("list_post")
-#     context = {
-#         'posts':posts,
-#         'r_posts':r_posts
-#     }
-#     return render(request,'Forum/list_post.html',context)
 
-# def detail_post(request,id):
-#     post = get_object_or_404(Post,id=id)
-#     r_posts = Post.objects.all().order_by('-id')[:4]
-#     comments = Comments.objects.filter(post=post)
-#     is_liked = False
-#     if post.likes.filter(id=request.user.id).exists():
-#         is_liked = True
-#     if request.method == 'POST':
-#             content = request.POST["content"]
-#             comment = Comments()
-#             comment.user = request.user
-#             comment.post = post
-#             comment.comment_content = content
-#             comment.save()
-#             messages.success(request,'Votre commentaire a été ajouté avec succès !')
-#     context = {
-#         'post':post,
-#         'r_posts':r_posts,
-#         'comments':comments,
-#         'is_liked':is_liked,
-#     }
-#     return render(request,'Forum/detail_post.html',context)
+def posts(request):
+    query = request.GET.get('q')
+    cat_checked = request.GET.get('cat')
+    word_checked = request.GET.get('tag')
+    posts_list = Post.objects.all()
+    r_posts = Post.objects.all().order_by('-id')[:3]
+    if word_checked:
+        tag_checked = Tag.objects.get(word=word_checked)
+        posts_list = Post.objects.filter(tags=tag_checked)
+    if cat_checked:
+        posts_list = Post.objects.filter(categorie=cat_checked)
+    if query:
+        posts_list = Post.objects.filter(
+            Q(title__icontains=query)|
+            Q(author__username__icontains=query)|
+            Q(content__icontains=query)
+        )
+    paginator = Paginator(posts_list,5)
+    page = request.GET.get('page')
+    try : 
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post()
+        post.author = request.user
+        post.title = title
+        post.content = content
+        post.status = status
+        try :
+            post.photo= request.FILES["photo"]
+        except :
+            print('Pas de photo de profil')
+        post.save()
+        return redirect("list_post")
+    context = {
+        'posts':posts,
+        'r_posts':r_posts
+    }
+    return render(request,'Blog_Forum/list_post.html',context)
 
-# def like_post(request):
-#     post_id = request.POST.get('post_id')
-#     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-#     is_liked = False
-#     if post.likes.filter(id=request.user.id).exists():
-#         post.likes.remove(request.user)
-#         is_liked = False
-#     else:
-#         post.likes.add(request.user)
-#         is_liked = True
-#     return redirect("detail_post",post_id)
+def detail_post(request,id):
+    post = get_object_or_404(Post,id=id)
+    r_posts = Post.objects.all().order_by('-id')[:4]
+    comments = Comments.objects.filter(post=post)
+    is_liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        is_liked = True
+    if request.method == 'POST' and request.user.is_authenticated:
+        content = request.POST["content"]
+        comment = Comments()
+        comment.user = request.user
+        comment.post = post
+        comment.comment_content = content
+        comment.save()
+        messages.success(request,'Votre commentaire a été ajouté avec succès !')
+    context = {
+        'post':post,
+        'r_posts':r_posts,
+        'comments':comments,
+        'is_liked':is_liked,
+    }
+    return render(request,'Blog_Forum/single-blog.html',context)
+
+@login_required
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    is_liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        is_liked = False
+    else:
+        post.likes.add(request.user)
+        is_liked = True
+    return redirect("detail_post",post_id)
 
     
         
