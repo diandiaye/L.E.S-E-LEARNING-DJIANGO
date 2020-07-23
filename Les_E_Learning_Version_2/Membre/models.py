f4956f20102b39e487da19a52ae8ef88fab804ab
falloudiakhate <falloudiakhate9@gmail.com>
2020-07-23 22:05:34 +0000

    Pulling Code

--- Les_E_Learning_Version_2/Membre/models.py
+++ Les_E_Learning_Version_2/Membre/models.py
@@ -4,11 +4,7 @@ from django.contrib.auth.models import User
 class Membre(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     niveau = models.CharField(max_length=50)
-<<<<<<< HEAD
-    def __str__(self):
-            return self.user.username
-    
-=======
+
     def __unicode__(self, ):
         return self.user.username  
->>>>>>> 5e67fc755225c7bdda9363cc838d3bda9669748a
+
