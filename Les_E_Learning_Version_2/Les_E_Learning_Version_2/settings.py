a84d3468ca291e335808fe55b4c6c7bcc6da6b1e
falloudiakhate <falloudiakhate9@gmail.com>
2020-08-20 20:06:48 +0000

    Blog

--- Les_E_Learning_Version_2/Les_E_Learning_Version_2/settings.py
+++ Les_E_Learning_Version_2/Les_E_Learning_Version_2/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'Templates')
 SECRET_KEY = '****************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
