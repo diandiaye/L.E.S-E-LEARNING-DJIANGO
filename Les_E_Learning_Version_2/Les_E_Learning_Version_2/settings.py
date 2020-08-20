10e99ae6b5f02f7cb38085710258c3f87c136da2
falloudiakhate <falloudiakhate9@gmail.com>
2020-08-20 13:12:34 +0000

    Prod Settings

--- Les_E_Learning_Version_2/Les_E_Learning_Version_2/settings.py
+++ Les_E_Learning_Version_2/Les_E_Learning_Version_2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '****************************************'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "34.66.236.137"]
 
 from django.contrib.messages import constants as messages
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
